9df09dec7086ab302ea7ec35977edad3ce280d09
Iury Dantas <macedo.iury@gmail.com>
2024-01-09 16:32:05 +0000

    tests

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,7 @@
 SELECT *,
     revenue - purchase_price AS margin
 FROM
-    {{ source('raw','sales') }}
-JOIN
-    {{ source('raw', 'product') }}
-USING(products_id)
+    {{ ref('stg_raw__sales') }}
+    JOIN
+    {{ ref('stg_raw__product') }}
+USING (products_id)
