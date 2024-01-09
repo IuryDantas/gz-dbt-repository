082742a3aa10a77da5ddf0adaf9ea77dd50bbc5d
Iury Dantas <macedo.iury@gmail.com>
2024-01-09 16:57:56 +0000

    test

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,12 @@
+SELECT orders_id
+    , date_date
+    , revenue
+    , quantity
+    , purchase_price
+    , ROUND(revenue-purchase_price,2) AS margin
+
+FROM
+    {{ ref('stg_raw__sales') }}
+JOIN
+    {{ ref('stg_raw__product') }}
+USING (products_id)
